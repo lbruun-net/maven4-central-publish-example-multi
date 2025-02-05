4d57c3e958a25f663fc379242bdf4f85c6090c74
Lars Bruun-Hansen <lbruun@apache.org>
2025-02-05 15:37:27 +0100

    Initialy

--- .github/scripts/maven-execution.sh
+++ .github/scripts/maven-execution.sh
@@ -13,10 +13,26 @@ set -e
 
 
 # Constants
+
+#
 # (Note: Projects which has registered with Sonatype/MavenCentral after February 2021 use
 #        "s01.oss.sonatype.org" as the hostname in the URLs below. Change accordingly.)
-mvn_central_release_url="https://oss.sonatype.org/service/local/staging/deploy/maven2/"
-mvn_central_snapshot_url="https://oss.sonatype.org/content/repositories/snapshots/"
+mvn_release_repo_url="https://oss.sonatype.org/service/local/staging/deploy/maven2/"
+mvn_snapshot_repo_url="https://oss.sonatype.org/content/repositories/snapshots/"
+mvn_release_repo_id="maven-central"
+mvn_snapshot_repo_id="${mvn_release_repo_id}"
+
+
+# SemVer regular expression:
+#  - The tag can optionally start with the 'v' but the 'v' doesn't become part of the Maven version string.
+#
+# -  We allow the X.Y.Z version to have a pre-release suffix, e.g. "3.2.0-RC1" but if
+#    so we tell Maven that this is a SNAPSHOT release. In other words: tag "3.2.0-RC1" will
+#    be published as version "3.2.0-RC1-SNAPSHOT" and will therefore go into the OSS Sonatype snapshot repo,
+#    not Maven Central.
+#
+semver_regex='^v?([0-9]+\.[0-9]+\.[0-9]+(\-[0-9a-zA-Z.]+)*)$'
+
 
 
 # Defaults
@@ -26,15 +42,7 @@ mvn_ci_sha1_short="${GITHUB_SHA::7}"
 mvn_ci_changelist=""
 mvn_profiles_active=""
 
-# SemVer regular expression:
-#  - The tag can optionally start with the 'v' but the 'v' doesn't become part of
-#    the Maven version string.
-# -  We allow the X.Y.Z version to have a pre-release suffix, e.g. "3.2.0-RC1" but if
-#    so we tell Maven that this is a SNAPSHOT release. In other words: tag "3.2.0-RC1" will
-#    be published as version "3.2.0-RC1-SNAPSHOT" and will therefore go into the OSS Sonatype snapshot repo,
-#    not Maven Central.
-#
-semver_regex='^v?([0-9]+\.[0-9]+\.[0-9]+(\-[0-9a-zA-Z.]+)*)$'
+
 
 if [ "$GITHUB_REF_TYPE" = "tag" ]; then
   # Does tag look like a SemVer string?
@@ -43,23 +51,23 @@ if [ "$GITHUB_REF_TYPE" = "tag" ]; then
       echo "A SemVer-like tag, \"$GITHUB_REF_NAME\", was pushed to GitHub independently of the GitHub Releases feature."
       echo "Releases must be created from the GitHub UI Releases page."
       exit 1
-    fi
-    semver=${BASH_REMATCH[1]}
-    semver_prerelease=${BASH_REMATCH[2]}
+    else
+      semver=${BASH_REMATCH[1]}
+      semver_prerelease=${BASH_REMATCH[2]}
 
-    mvn_phase="deploy"
-    mvn_ci_revision="$semver"
-    mvn_ci_sha1_short=""
-    mvn_profiles_active="-Prelease-to-central"
+      mvn_phase="deploy"
+      mvn_ci_revision="$semver"
+      mvn_ci_sha1_short=""
+      mvn_profiles_active="-Prelease-to-central"
 
-    # Test for SemVer pre-releases. We turn those into SNAPSHOTs
-    if [ ! -z "$semver_prerelease" ]; then
-      # Unless "SNAPSHOT" is already the Semver Pre-release string, then..
-      if [[ ! "$semver_prerelease" =~ SNAPSHOT$ ]]; then
-        mvn_ci_changelist="-SNAPSHOT"  # effectively, this gets appended to the full Maven version string
+      # Test for SemVer pre-releases. We turn those into SNAPSHOTs
+      if [ -n "$semver_prerelease" ]; then
+        # Unless "SNAPSHOT" is already the Semver Pre-release string, then..
+        if [[ ! "$semver_prerelease" =~ SNAPSHOT$ ]]; then
+          mvn_ci_changelist="-SNAPSHOT"  # effectively, this gets appended to the full Maven version string
+        fi
       fi
     fi
-
   else
     # Tagged with something not resembling a SemVer string.
     # This may be a mistake. But we accept it as there may be valid reasons for creating Git tags not related to
@@ -83,7 +91,7 @@ fi
 #   - 'deployAtEnd':  For multi-module builds this is important. We either want to deploy
 #                     all modules or none. (i.e. the deploy should ideally be one atomic action)
 #
-#   - We expect a settings.xml file to exist which defines credentials for a Server named 'maven-central'
+#   - We expect a settings.xml file to exist which defines credentials for a Server with id 'maven-central'
 #
 set -x
 ./mvnw \
@@ -91,11 +99,10 @@ set -x
   --non-interactive \
   --no-transfer-progress \
   -DdeployAtEnd=true \
-  -DaltReleaseDeploymentRepository=maven-central::$mvn_central_release_url \
-  -DaltSnapshotDeploymentRepository=maven-central::$mvn_central_snapshot_url \
+  -DaltReleaseDeploymentRepository=${mvn_release_repo_id}::$mvn_release_repo_url \
+  -DaltSnapshotDeploymentRepository=${mvn_snapshot_repo_id}::$mvn_snapshot_repo_url \
   -Dci.project.url="${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}" \
   -Dchangelist=$mvn_ci_changelist  -Dsha1=$mvn_ci_sha1_short  -Drevision=$mvn_ci_revision \
   $mvn_profiles_active \
-  help:active-profiles \
   $mvn_phase
 
